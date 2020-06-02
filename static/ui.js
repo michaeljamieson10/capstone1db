495c18f2235f694bd058358fbbe0931801f94b90
michael <michael@michaels-MBP.fios-router.home>
2020-06-01 20:47:02 -0400

    added a alert for when medication is given that fades out

--- static/ui.js
+++ static/ui.js
@@ -9,7 +9,7 @@ $(document).on("click", ".patient", async function(event) {
     const med = await getPatientId(patient_id)
     medHTML = generateMedicationHTML(med.data)
     $(this).siblings().css('background', '#ffffff')
-    $(this).css('background', '#ccc'); 
+    $(this).css('background', '#1556d6'); 
     $('.medication-emr').append(medHTML);
 })
 
@@ -28,8 +28,7 @@ function generateMedicationHTML(medicationArray) {
             <h5 class="card-title">${med.name}</h5>
             <a href="/medications/${med.id}/patients/${med.patients_id}/given">View History</a>
             <p class="card-text">${med.description}</p>
-            <a class="btn btn-primary given" data-medId=${med.id} data-ptId=${med.patients_id}>Give</a>
-            <span id="ifGiven"></span
+            <a class="btn btn-primary given" data-medName=${med.name} data-medId=${med.id} data-ptId=${med.patients_id}>Give</a>
             </div>
         </div>
 
@@ -46,10 +45,16 @@ function generateMedicationHTML(medicationArray) {
 
 $(document).on("click", ".given", async function(event) {
     event.preventDefault();
-    medication_id = $(this).attr('data-medId')
-    patients_id = $(this).attr('data-ptId')
-    await createGivenMedication(patients_id,medication_id)
-    // $('#ifGiven').text("Given")
+    console.log("this buttonw orks")
+    medication_id = $(this).attr('data-medId');
+    patients_id = $(this).attr('data-ptId');
+    medication_name = $(this).attr('data-medName');
+    await createGivenMedication(patients_id,medication_id);
+    $('.medication-emr').prepend(`<div id="given_${medication_id}"class="alert alert-success" role="alert">${medication_name} was given</div>`)
+    let given_med = $(`#given_${medication_id}`)
+    setTimeout(function(){
+        given_med.fadeOut('slow')
+    },3000)
 });
 
 /** 
